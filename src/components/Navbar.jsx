010f1aa929fa15f38fba074392ab44ada291c5e2
geor1812 <george.ursachi7@gmail.com>
2020-12-09 16:44:59 +0100

    fixed some css, added refresh button into logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import favicon from "../images/logo.ico"
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.refreshPage = this.refreshPage.bind(this);
+    }
+
+    refreshPage() {
+        window.location.reload();
+    }
+
     render() {
         return(
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a className="navbar-brand" href="#">Websites2</a>
-
+                    <input type="image" className="img-fluid mx-sm-3" width="50" height="50" src={favicon} alt="responsive logo" onClick={this.refreshPage}/>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
